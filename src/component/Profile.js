a546eb2a57e7c7d8c49b0eed45e2e4f43f5aabde
Prakash L <121917226+prakashlohanathan@users.noreply.github.com>
2023-06-03 12:08:46 +0530

    Add files via upload

---
+++ src/component/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function Profile() {
+  return <>
+    <div>
+        <h1>Profile</h1>
+<h3>The Sub Component are:</h3>
+
+<ul>
+    <Link to='details'><li>Profile details</li></Link>
+    <Link to='reset-password'><li>Reset Password</li></Link>
+</ul>
+<Outlet/>
+</div>
+</>
+}
+
+export default Profile
---
+++ src/component/Sidebar.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+
+function Sidebar() {
+    let navigate = useNavigate()
+  return <>
+    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+{/* <!-- Sidebar - Brand --> */}
+<div className="sidebar-brand d-flex align-items-center justify-content-center">
+    <div className="sidebar-brand-icon rotate-n-15">
+        <i className="fas fa-laugh-wink"></i>
+    </div>
+    <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+</div>
+
+{/* <!-- Divider --> */}
+<hr className="sidebar-divider my-0"/>
+
+{/* <!-- Nav Item - Dashboard --> */}
+<Link to='/dashboard'>
+<li className="nav-item active">
+    <div className="nav-link">
+    <i className="fas fa-fw fa-tachometer-alt"></i>
+    <span>Dashboard</span></div>
+</li>
+</Link>
+
+{/* <!-- Divider --> */}
+<hr className="sidebar-divider"/>
+
+{/* <!-- Heading --> */}
+<div className="sidebar-heading">
+    Interface
+</div>
+
+{/* <!-- Nav Item - Pages Collapse Menu --> */}
+<Link to='/add-user'>
+<li className="nav-item">
+    <div className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fa-solid fa-user-plus"></i>
+        <span>Add User</span>
+    </div>
+</li>
+</Link>
+
+<Link to='/profile'>
+<li className="nav-item">
+    <div className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+       <i class="fa-solid fa-address-card"></i>
+        <span>Profile</span>
+    </div>
+</li>
+</Link>
+
+<Link to='/pending-request'>
+<li className="nav-item">
+    <div className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fa-solid fa-rotate"></i>
+        <span>Pending Request</span>
+    </div>
+</li>
+</Link>
+
+
+<li className="nav-item" onClick={()=>navigate('/task')}>
+    <div className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fa-solid fa-list-check"></i>
+        <span>Task</span>
+    </div>
+</li>
+</ul>
+  </>
+}
+
+export default Sidebar
